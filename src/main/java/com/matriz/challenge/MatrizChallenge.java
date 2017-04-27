fa13b6cc46c9106579b198ea882075f6caae897e
carloslimasis <carloslimasis@gmail.com>
2017-04-27 14:23:58 -0300

    Update files

--- src/main/java/com/matriz/challenge/MatrizChallenge.java
+++ src/main/java/com/matriz/challenge/MatrizChallenge.java
@@ -4,7 +4,6 @@ import static java.lang.Integer.parseInt;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
